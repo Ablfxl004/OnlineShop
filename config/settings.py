3af67374116eacec8ee6901a5a7af8358754b527
Abolfazl Taghavi <abolfazl.taghavi004@gmail.com>
2025-03-13 23:19:24 +0330

    add _base.html and install crispy form then use it

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     'accounts',
     'pages',
 ]
@@ -140,4 +143,5 @@ LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
